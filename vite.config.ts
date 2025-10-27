8aa2ac66eb8fdf5fae3841bd7908cf6d998aa593
“Ygor <ygorkayan@icloud.com>
2025-10-27 17:14:14 +0000

    feature: adding new react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import babel from "vite-plugin-babel";
 
 export default defineConfig({
   plugins: [
     react(),
+    babel({
+      babelConfig: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
   ],
 });
