2e5a22924fc7b44369930b39b06244ccba3d3cd7
“Ygor <ygorkayan@icloud.com>
2026-01-29 15:11:45 +0000

    feature validating token

--- src/app/login/service.ts
+++ src/app/login/service.ts
@@ -1,7 +1,7 @@
-export const login = async (user: string, password: string) => {
-  const url = "https://studies-back.ygorkayan.workers.dev/login";
+import { URL_LOGIN } from "../../global";
 
-  const response = await fetch(url, {
+export const login = async (user: string, password: string) => {
+  const response = await fetch(URL_LOGIN, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -16,4 +16,23 @@ export const login = async (user: string, password: string) => {
   const data = await response.json();
 
   return { logged: true, token: data.body };
+};
+
+export const checkToken = async (token: string | null) => {
+  if (token === null) {
+    return false;
+  }
+
+  const res = await fetch(URL_LOGIN, {
+    method: "GET",
+    headers: {
+      token: token || "",
+    },
+  });
+
+  if (res.status === 200) {
+    return true;
+  }
+
+  return false;
 };
